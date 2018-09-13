d422597b04c2f919275fe6e1b87449daaf819311
annatea <anna_abrahamyan@edu.aua.am>
2018-09-13 13:52:55 +0300

    first commit

---
+++ homework2.js
@@ -0,0 +1,26 @@
+<script>
+  const a=true;
+  const b=1;
+  const c=5;
+  const petros='aua';
+  const z=4;
+
+  console.log(((z+c)-1)/2);
+
+  console.log((5*1)-b === z);
+
+  if(petros !== z || !!!c<'aua'+b) {
+    console.log('Conngrats!You're a wizard');
+  }
+   else{
+     console.log('Not this time, muggle!');
+  }
+
+
+  if( petros='aua' === a && z-5 === b) {
+    console.log('Happy Birthday');
+  }
+   else{
+     console.log('R.I.P.');
+  }
+</script>
